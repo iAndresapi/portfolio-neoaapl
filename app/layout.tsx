4141f1020620e6c33f1e481a2ff2a178e550bf29
Arturo Patiño <pandresarturo@gmail.com>
2025-08-13 12:44:52 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Portfolio- NeoAAPL',
+  title: 'Portfolio - NeoAAPL',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
